ce7101736ded9b6f907d437f68f3b504f6c67080
Fathya Ariyani <fathyariyani@gmail.com>
2025-06-17 13:17:18 +0700

    change to url prod

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -173,7 +173,7 @@ const resolvers = {
   RekamMedis: {
     pasien: async (parent) => {
       try {
-        const res = await fetch('http://data-individu-service:8000/graphql', {
+        const res = await fetch('https://gtw-data-individu-service.up.railway.app/graphql', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
